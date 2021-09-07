b4c5739a025d81a2e21d0c2b788090986952794e
DIEGO ROSADO cabrera <rocadie9615@ciencias.unam.mx>
2021-09-07 20:55:32 +0100

    llegue hasta donde pude :C

--- src/main/java/com/mitocode/di/empleado/service/Excersice4Service.java
+++ src/main/java/com/mitocode/di/empleado/service/Excersice4Service.java
@@ -1,5 +1,9 @@
 package com.mitocode.di.empleado.service;
 
+import java.util.List;
+
+import com.mitocode.di.empleados.dto.Employees;
+
 public interface Excersice4Service {
-	public double getSalary();
+	public double getSalary(List <Employees> exployers,Employees e ,String fI,String fF);
 }
--- src/main/java/com/mitocode/di/empleado/service/Excersise4ServiceImpl.java
+++ src/main/java/com/mitocode/di/empleado/service/Excersise4ServiceImpl.java
@@ -1,5 +1,14 @@
 package com.mitocode.di.empleado.service;
 
-public class Excersise4ServiceImpl {
+import java.util.List;
 
+import com.mitocode.di.empleados.dto.Employees;
+import com.mitocode.di.empleados.repository.Excercie4Repository;
+
+public class Excersise4ServiceImpl implements Excersice4Service {
+	Excercie4Repository excercie4Repository;
+	@Override
+	public double getSalary(List <Employees> exployers,Employees e ,String fI,String fF) {
+		return excercie4Repository.getPay(e, fF, fI);
+	}
 }
